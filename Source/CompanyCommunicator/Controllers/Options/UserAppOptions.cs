ee7877acff3ccc2e897f98b29a7b05d3f38e0a87
Riddhi Chaplot <riddhichaplot@gmail.com>
2022-04-14 17:35:01 +0530

    Signed-off-by: Riddhi Chaplot <riddhichaplot@gmail.com>

---
+++ Source/CompanyCommunicator/Controllers/Options/UserAppOptions.cs
@@ -0,0 +1,33 @@
+﻿// <copyright file="UserAppOptions.cs" company="Microsoft">
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+// </copyright>
+
+namespace Microsoft.Teams.Apps.CompanyCommunicator.Controllers.Options
+{
+    /// <summary>
+    /// User app options.
+    /// </summary>
+    public class UserAppOptions
+    {
+        /// <summary>
+        /// Gets or sets a value indicating whether user app should be proactively installed.
+        /// </summary>
+        public bool ProactivelyInstallUserApp { get; set; }
+
+        /// <summary>
+        /// Gets or sets User app's external Id (id in the manifest).
+        /// </summary>
+        public string UserAppExternalId { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the images should be uploaded to Azure Blob Storage.
+        /// </summary>
+        public bool ImageUploadBlobStorage { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating how much time the SAS token is valid to access the images uploaded to Azure Blob Storage.
+        /// </summary>
+        public int ImageUploadBlobStorageSasDurationHours { get; set; }
+    }
+}
